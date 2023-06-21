e0bc170fc0104d7341936118545574e21876e233
tonayam <tonayam99@gmail.com>
2023-06-21 16:00:50 +0100

    About Page Updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,10 @@
+// IMAGES
 import sub1 from '../assets/sub-1.jpg';
 import sub2 from '../assets/sub-2.jpg';
+import reviewer1 from '../assets/reviewer-1.jpg';
+
+// ICONS
+import { BsFillStarFill } from 'react-icons/bs';
 
 export const subsidiaries = [
   {
@@ -43,5 +48,29 @@ export const subsidiaries = [
       `Make your business visible online.`,
       `Make your business visible online.`,
     ],
+  },
+];
+
+export const reviews = [
+  {
+    stars: [<BsFillStarFill />, <BsFillStarFill />, <BsFillStarFill />],
+    comment: `I have been hiring people in this space for a number of years and I have never seen this level of professionalism. It really feels like you are working with a team that can get the job done.`,
+    reviewerName: `Leonard Heiser`,
+    reviewerPosition: `ceo`,
+    reviewerPhoto: reviewer1,
+  },
+  {
+    stars: [<BsFillStarFill />, <BsFillStarFill />, <BsFillStarFill />],
+    comment: `I have been hiring people in this space for a number of years and I have never seen this level of professionalism. It really feels like you are working with a team that can get the job done.`,
+    reviewerName: `Micheal Angelo`,
+    reviewerPosition: `ceo`,
+    reviewerPhoto: reviewer1,
+  },
+  {
+    stars: [<BsFillStarFill />, <BsFillStarFill />, <BsFillStarFill />],
+    comment: `I have been hiring people in this space for a number of years and I have never seen this level of professionalism. It really feels like you are working with a team that can get the job done.`,
+    reviewerName: `Leonardo Davinci`,
+    reviewerPosition: `Customer`,
+    reviewerPhoto: reviewer1,
   },
 ];
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Marquee from 'react-fast-marquee';
 import aboutUs1 from '../../assets/about-us-1.jpg';
 import aboutUs2 from '../../assets/about-us-2.jpg';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { reviews } from '../../data/data';
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const reviewsWrapperHeight = `${reviews.length * 50}vh`;
+  const triggerRef = useRef(null);
+  const sectionRef = useRef(null);
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      { translateY: 0 },
+      {
+        translateY: `-${reviews.length * 50 - 50}vh`,
+        ease: `none`,
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: `top top`,
+          end: `1000 top`,
+          pin: true,
+          scrub: 0.6,
+        },
+      }
+    );
+
+    return () => pin.kill();
+  });
+
   return (
     <main className='about-page'>
       <header>
@@ -59,6 +84,77 @@ const About = () => {
               clients which drive super-charge improvements in organic rankings,
               increase visits and grow revenue.
             </p>
+          </div>
+        </div>
+      </section>
+
+      {/* THE JOURNEY/REVIEWS SECTION */}
+      <section ref={triggerRef} className='journey-reviews'>
+        {/* the journey */}
+        <div className='journey'>
+          <h5 className='sub-title'>SINCE FROM 2008</h5>
+          <h2>
+            <span>Keep pushing forward. </span>
+            <span> We've got your back.</span>
+          </h2>
+          <p>
+            Things go wrong have questions. We've understand. So we have people
+          </p>
+          <div className='divider'></div>
+          <div className='stats'>
+            <div className='stat'>
+              <h3>12K</h3>
+              <p>
+                <span>Happy Users</span>
+                <span>Around World</span>
+              </p>
+            </div>
+            <div className='stat'>
+              <h3>12K</h3>
+              <p>
+                <span>Happy Users</span>
+                <span>Around World</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* reviews */}
+        <div className='reviews'>
+          <div
+            ref={sectionRef}
+            style={{ height: reviewsWrapperHeight }}
+            className='wrapper'
+          >
+            {reviews.map((review, reviewIndex) => {
+              const {
+                comment,
+                reviewerName,
+                reviewerPhoto,
+                reviewerPosition,
+                stars,
+              } = review;
+
+              return (
+                <div className='review' key={reviewIndex}>
+                  <div className='stars'>
+                    {stars.map((star, starIndex) => {
+                      return <div key={starIndex}>{star}</div>;
+                    })}
+                  </div>
+                  <p>{comment}</p>
+                  <div className='reviewer'>
+                    <div className='profile'>
+                      <img src={reviewerPhoto} alt={reviewerName} />
+                    </div>
+                    <div className='info'>
+                      <h4>{reviewerName}</h4>
+                      <h6>{reviewerPosition}</h6>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
